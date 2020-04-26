fa2d91e45e9716d476695f5c62e58392a3993281
Par <pacroy@gmail.com>
2020-04-26 10:01:34 +0700

    Added terraform as tf

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
+
 # Azure CLI
 alias azset='az account set -s'
 alias azlist='az account list -o table'
@@ -8,3 +9,6 @@ alias kc='kubectl'
 alias kclist='kc config get-contexts'
 alias kcuse='kc config use-context'
 kcsetns() { context=$(kc config current-context); kc config set-context $context --namespace $1; }
+
+# Terraform
+alias tf='terraform'

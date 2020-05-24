dc5d3c8857be548739149a6f7dae0859536f3504
Par <pacroy@gmail.com>
2020-05-24 03:09:04 +0000

    Added kcrollout

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,7 @@ alias kc='kubectl'
 alias kclist='kc config get-contexts'
 alias kcuse='kc config use-context'
 kcsetns() { context=$(kc config current-context); kc config set-context $context --namespace $1; }
+kcrollout() { kc rollout status deploy $1; }
 
 # Terraform
 alias tf='terraform'

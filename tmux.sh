344da237af29cd7787274d06120068537617d996
Par <pacroy@gmail.com>
2020-07-01 12:28:27 +0000

    Added apt-check in tmux

--- tmux.sh
+++ tmux.sh
@@ -6,6 +6,10 @@
 if [ -x "$(command -v tmux)" ]; then
   if ! { [[ "$TERM" == screen* ]] && [ -n "$TMUX" ]; } then
     tmux
+  else
+    if [ -f "/usr/lib/update-notifier/apt-check" ]; then
+      /usr/lib/update-notifier/apt-check --human-readable
+    fi
   fi
 fi
 # ===== END CUSTOM SCRIPT =====

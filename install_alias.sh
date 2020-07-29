52eee05eaa771e5244c7aabe3c9e041cc597b0d0
Par <chairat.onyaem@exxonmobil.com>
2020-07-29 19:51:49 +0700

    Move scripts back to home

--- install_alias.sh
+++ install_alias.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 set -e
 
-curl -sS https://raw.githubusercontent.com/pacroy/bash-alias/master/alias.sh -o ~/clouddrive/alias.sh
-echo -en "\nsource ~/clouddrive/alias.sh\nexport PACROY_ALIAS=true\n" >> ~/.bashrc
-source ~/clouddrive/alias.sh
+curl -sS https://raw.githubusercontent.com/pacroy/bash-alias/master/alias.sh -o ~/alias.sh
+echo -en "\nsource ~/alias.sh\nexport PACROY_ALIAS=true\n" >> ~/.bashrc
+source ~/alias.sh
--- install_tmux.sh
+++ install_tmux.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 set -e
 
-curl -sS https://raw.githubusercontent.com/pacroy/bash-alias/master/tmux.sh -o ~/clouddrive/tmux.sh
-echo -en "\nsource ~/clouddrive/tmux.sh\nexport PACROY_TMUX=true\n" >> ~/.bashrc
-source ~/clouddrive/tmux.sh
+curl -sS https://raw.githubusercontent.com/pacroy/bash-alias/master/tmux.sh -o ~/tmux.sh
+echo -en "\nsource ~/tmux.sh\nexport PACROY_TMUX=true\n" >> ~/.bashrc
+source ~/tmux.sh
